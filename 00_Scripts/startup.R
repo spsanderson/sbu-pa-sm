91bca0912ebdcefd513e69db787b6e0c7db7e736
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-07-10 08:50:50 -0400

    Update startup.R

--- 00_Scripts/startup.R
+++ 00_Scripts/startup.R
@@ -1,6 +1,9 @@
 source("00_Scripts/library_load.R")
+source("00_Scripts/data_create.R")
 source("02_Data_Manipulation/data_functions.R")
 source("03_Viz/viz_functions.R")
 source("04_TS_Modeling/ts_functions.R")
 
 library_load()
+data_create() %>%
+    saveRDS(file = "00_Data/data_tbl.RDS")
