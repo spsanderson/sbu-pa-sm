71f72713ca555fb651e4bfa229d85d8904c414cc
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-07-10 08:50:47 -0400

    Update main.R

--- 00_Scripts/main.R
+++ 00_Scripts/main.R
@@ -1,1 +1,9 @@
+
+# Source Startup File -----------------------------------------------------
+
+
 source("00_Scripts/startup.R")
+
+# Read Data ---------------------------------------------------------------
+
+df_tbl <- readRDS("00_Data/data_tbl.RDS")
