7603fbc39f757880b5c7fa5828c6eb22013c2a1a
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-07-10 08:50:39 -0400

    Update data_create.R

---
+++ 00_Scripts/data_create.R
@@ -0,0 +1,27 @@
+
+# Get Data ----------------------------------------------------------------
+
+data_create <- function() {
+    df_tbl <- healthyR_data %>%
+        as_tibble() %>%
+        select(
+            mrn,
+            visit_id,
+            visit_start_date_time,
+            visit_end_date_time,
+            total_charge_amount,
+            total_adjustment_amount,
+            total_amount_due,
+            total_payment_amount,
+            payer_grouping,
+            service_line
+        ) %>%
+        mutate(
+            adm_date = as.Date(visit_start_date_time),
+            dsch_date = as.Date(visit_end_date_time)
+        )
+    
+    return(df_tbl)
+}
+
+
